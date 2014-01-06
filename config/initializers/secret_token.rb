5699dbc9d255917531d0e5757b84a9119e2f4847
Isaac Murchie <isaac@saucelabs.com>
2014-01-06 14:40:20 -0800

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SauceGuineaApp::Application.config.secret_key_base = '69ba9509e1448a8181c1e02cce7845a03cf055073daed7f7c27ef513c15e47e2856bd7070e20449d0808109e910814f86baae15416497083476a9906aa684d08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SauceGuineaApp::Application.config.session_store :cookie_store, key: '_sauce_guinea_app_session'
